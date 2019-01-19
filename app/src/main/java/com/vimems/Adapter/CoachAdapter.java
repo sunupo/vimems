b77a5a726630b9278abbf8c9cd327a1fd7f80da1
sunupo163 <sunupo163@163.com>
2019-01-19 18:13:15 +0800

    coachAdapter还没写完

---
+++ app/src/main/java/com/vimems/Adapter/CoachAdapter.java
@@ -0,0 +1,39 @@
+package com.vimems.Adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.vimems.AdapterItem.CoachItem;
+
+import java.util.List;
+
+/**
+ * author sunupo
+ * date 2019/1/19 14:00
+ * description 
+ * 
+ */
+
+public class CoachAdapter extends RecyclerView.Adapter<CoachAdapter.ViewHolder>{
+
+
+    @NonNull
+    @Override
+    public CoachAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CoachAdapter.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
