cbc13f29f6b82ccc58928d4a75d20e055f718944
sunupo163 <sunupo163@163.com>
2019-01-19 23:14:58 +0800

    first

--- app/src/main/java/com/vimems/Adapter/CoachAdapter.java
+++ app/src/main/java/com/vimems/Adapter/CoachAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.vimems.AdapterItem.CoachItem;
 import com.vimems.R;
@@ -25,10 +26,13 @@ public class CoachAdapter extends RecyclerView.Adapter<CoachAdapter.ViewHolder>{
     private ArrayList<CoachItem> coachItemArrayList;
 
     static class ViewHolder extends RecyclerView.ViewHolder{
+        View coachView;
         ImageView coachImage;
         TextView coachName;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            coachView=itemView;
             coachImage= itemView.findViewById(R.id.coach_image);
             coachName= itemView.findViewById(R.id.coach_name);
         }
@@ -43,7 +47,23 @@ public class CoachAdapter extends RecyclerView.Adapter<CoachAdapter.ViewHolder>{
     @Override
     public CoachAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View itemView= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_coach,viewGroup,false);
-        ViewHolder holder=new ViewHolder(itemView);
+        final ViewHolder holder=new ViewHolder(itemView);
+        holder.coachName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position=holder.getAdapterPosition();
+                CoachItem coachItem=coachItemArrayList.get(position);
+                Toast.makeText(v.getContext(),"you have clicked view"+coachItem.getName(),Toast.LENGTH_LONG).show();
+            }
+        });
+        holder.coachView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position=holder.getAdapterPosition();
+                CoachItem coachItem=coachItemArrayList.get(position);
+                Toast.makeText(v.getContext(),"教练是"+coachItem.getName(),Toast.LENGTH_LONG).show();
+            }
+        });
         return holder;
     }
 
